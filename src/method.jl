ded73e2f2dc296f55f9f0126cdf29f83dc0cfeb0
Filipe Braida <filipebraida@gmail.com>
2017-09-14 18:40:00 -0300

    Adding normalization feature

--- src/method.jl
+++ src/method.jl
@@ -1,17 +1,43 @@
-mutable struct Cofils <: Persa.CFModel
+abstract type Cofils <: Persa.CFModel end
+
+mutable struct GlobalCofils <: Cofils
     slmodel::Nullable{DecisionTree.Ensemble}
     vl::VariableLatent
     features::Int
-    usersmean::Array{Float64}
+    preferences::Persa.RatingPreferences
+end
+
+mutable struct UserCofils <: Cofils
+    slmodel::Nullable{DecisionTree.Ensemble}
+    vl::VariableLatent
+    features::Int
+    means::Array{Float64}
+    preferences::Persa.RatingPreferences
+end
+
+mutable struct ItemCofils <: Cofils
+    slmodel::Nullable{DecisionTree.Ensemble}
+    vl::VariableLatent
+    features::Int
+    means::Array{Float64}
     preferences::Persa.RatingPreferences
 end
 
 uservl(model::Cofils, id::Int) = user(model.vl, id)
 itemvl(model::Cofils, id::Int) = item(model.vl, id)
 
-function Cofils(dataset::Persa.CFDatasetAbstract, features::Int, extractvl::Function = svd)
-    usersmean = zeros(dataset.users)
-    return Cofils(Nullable{DecisionTree.Ensemble}(), extractvl(dataset, features), features, Persa.shrunkUserMean(dataset, 10), dataset.preferences)
+function Cofils(dataset::Persa.CFDatasetAbstract, features::Int; normalization::Symbol = :user, extractvl::Function = svd)
+    @assert in(normalization, [:user, :item, :none]) "Incorrect value of normalization. Use :user, :item or :none."
+
+    if normalization == :user
+        means = Persa.means(dataset; mode = :user, α = 10)
+        return UserCofils(Nullable{DecisionTree.Ensemble}(), extractvl(dataset, features), features, means, dataset.preferences)
+    elseif normalization == :item
+        means = Persa.means(dataset; mode = :item, α = 10)
+        return ItemCofils(Nullable{DecisionTree.Ensemble}(), extractvl(dataset, features), features, means, dataset.preferences)
+    end
+
+    return GlobalCofils(Nullable{DecisionTree.Ensemble}(), extractvl(dataset, features), features, dataset.preferences)
 end
 
 function Persa.train!(model::Cofils, dataset::Persa.CFDatasetAbstract; nfeaturestrees::Int = 10, trees::Int = 20)
@@ -27,7 +53,7 @@ function Persa.train!(model::Cofils, dataset::Persa.CFDatasetAbstract; nfeatures
     return nothing
 end
 
-function convert2sl(model::Cofils, dataset::Persa.CFDatasetAbstract)
+function convert2sl(model::GlobalCofils, dataset::Persa.CFDatasetAbstract)
     m, n = size(model.vl)
 
     attributes = Array{Float64,2}(length(dataset), m + n)
@@ -36,9 +62,40 @@ function convert2sl(model::Cofils, dataset::Persa.CFDatasetAbstract)
     for i=1:length(dataset)
       (u, v, r) = dataset[i]
 
-      attributes[i,1:m] = uservl(model, u)
-      attributes[i,(m+1):(m+n)] = itemvl(model, v)
-      labels[i] = r - model.usersmean[u]
+      attributes[i, :] = convert2sl(model, u, v)
+      labels[i] = r
+    end
+
+    return attributes, labels
+end
+
+function convert2sl(model::UserCofils, dataset::Persa.CFDatasetAbstract)
+    m, n = size(model.vl)
+
+    attributes = Array{Float64,2}(length(dataset), m + n)
+    labels = Array{Float64,1}(length(dataset))
+
+    for i=1:length(dataset)
+      (u, v, r) = dataset[i]
+
+      attributes[i, :] = convert2sl(model, u, v)
+      labels[i] = r - model.means[u]
+    end
+
+    return attributes, labels
+end
+
+function convert2sl(model::ItemCofils, dataset::Persa.CFDatasetAbstract)
+    m, n = size(model.vl)
+
+    attributes = Array{Float64,2}(length(dataset), m + n)
+    labels = Array{Float64,1}(length(dataset))
+
+    for i=1:length(dataset)
+      (u, v, r) = dataset[i]
+
+      attributes[i, :] = convert2sl(model, u, v)
+      labels[i] = r - model.means[v]
     end
 
     return attributes, labels
@@ -55,10 +112,26 @@ function convert2sl(model::Cofils, user::Int, item::Int)
     return attributes
 end
 
-function Persa.predict(model::Cofils, user::Int64, item::Int64)
+function Persa.predict(model::GlobalCofils, user::Int64, item::Int64)
     attributes = convert2sl(model, user, item)
 
-    p = apply_forest(get(model.slmodel), attributes)[1] + model.usersmean[user]
+    p = apply_forest(get(model.slmodel), attributes)[1]
+
+    return Persa.correct(p, model.preferences)
+end
+
+function Persa.predict(model::UserCofils, user::Int64, item::Int64)
+    attributes = convert2sl(model, user, item)
+
+    p = apply_forest(get(model.slmodel), attributes)[1] + model.means[user]
+
+    return Persa.correct(p, model.preferences)
+end
+
+function Persa.predict(model::ItemCofils, user::Int64, item::Int64)
+    attributes = convert2sl(model, user, item)
+
+    p = apply_forest(get(model.slmodel), attributes)[1] + model.means[item]
 
     return Persa.correct(p, model.preferences)
 end
